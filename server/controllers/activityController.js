66cc230b6983ed8d0684c252cf82bc7b8710eb65
michamoe <98765003+michamoe@users.noreply.github.com>
2023-10-23 15:13:15 +0200

    activity

---
+++ server/controllers/activityController.js
@@ -0,0 +1,32 @@
+import Activity from "../models/Activity.js";
+
+export const getActivities = async (req, res, next) => {
+  try {
+    const activities = await Activity.find();
+    console.log("Activities:", activities);
+
+    if (!activities.length) {
+      throw { statusCode: 404, message: "Activity not found" };
+    }
+    res.json(activities);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const getActivityById = async (req, res, next) => {
+  try {
+    const { id } = req.params; 
+    const activity = await Activity.findOne({ id }); 
+
+    if (!activity) {
+     
+      return res.status(404).json({ message: "Activity not found" });
+    }
+
+    res.json(activity); 
+  } catch (error) {
+    next(error);
+  }
+};
--- server/models/Activity.js
+++ server/models/Activity.js
@@ -6,20 +6,49 @@ const activitySchema = new mongoose.Schema({
   title: {
     type: String,
     required: [true, "Title is required"],
+    trim: true,
   },
   description: {
     type: String,
     required: [true, "Description is required"],
   },
-  active: {},
-  startDate: {},
-  minSlots: {},
-  maxSlots: {},
-  requirements: {},
-  address: {},
-  images: [{}],
-  price: {},
-  category: [{}],
-  publishedDate: {},
+  active: {
+    type: Boolean,
+    default: false,
+  },
+  startDate: {
+    type: Date,
+  },
+  minSlots: {
+    type: Number,
+  },
+  maxSlots: {
+    type: Number,
+  },
+  requirements: [{
+    type: String,
+  }],
+  address: {
+    Street: {type: String},
+    Housenumber: {type: Number},
+    ZIP: {type: Number},
+    City: {type: String},
+    Country: {type: String},
+  },
+  images: [{
+    url: {type: String},
+    alt: {type: String},
+  }],
+  price: {
+    type: Number,
+    default: 0,
+  },
+  category: [{
+    type: String,
+  }],
+  publishedDate: {
+    type: Date,
+    default: Date.now,
+  },
 });
 export default mongoose.model("Activity", activitySchema);
