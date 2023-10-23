642a07d3d3ffabb72d39a0b95fa4217dca022e27
KhalidMohamad <125975613+KhalidMohamad@users.noreply.github.com>
2023-10-23 11:25:42 +0200

    client pages and app routes with react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,10 @@ import NavBar from "./components/Navbar/NavBar.jsx";
 import HomePage from "./components/HomePage/HomePage.jsx";
 import LogIn from "./components/User/LogIn.jsx";
 import SignUp from "./components/User/SignUp.jsx";
-
+import CoursePage from "./components/CoursePage/CoursePage.jsx";
+import NotFound from "./components/NotFound.jsx";
+import CourseDetails from "./components/CoursePage/CourseDetails.jsx";
+import {Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -10,11 +13,15 @@ function App() {
   return (
     <>
       <NavBar />
-      <HomePage />
-      -----!!!!!!LOG-IN FELD !!!!!!-----
-      <LogIn />
-      !!!!!!-------UNTERES FELD SIGN UP--------!!!!
-      <SignUp />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/activities" element={<CoursePage />} />
+        <Route path="/activities/:id" element={<CourseDetails />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
     </>
   );
 }
