b906f5d6e997520b80d034ff29c30bdca57aeb1f
michamoe <98765003+michamoe@users.noreply.github.com>
2023-10-24 11:30:25 +0200

    booking try

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRouter.js";
 import userRouter from "./routes/userRouter.js";
 import activityRouter from "./routes/activityRouter.js";
+import bookingRouter from "./routes/bookingRouter.js";
 import "./db/server.js";
 
 const PORT = process.env.PORT || 8000;
@@ -22,6 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", authRouter);
 app.use("/user", userRouter);
 app.use("/activities", activityRouter);
+app.use("/bookings", bookingRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${HOST}:${PORT}`);
