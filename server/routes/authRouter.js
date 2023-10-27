b29e37dee4622c0cb18430d1f48d23a495828f0d
Sven Grundmann <sgrundmannwbs@gmail.com>
2023-10-27 11:15:07 +0200

    newbackend with sql

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -4,9 +4,7 @@ import * as authController from "../controllers/authController.js";
 const authRouter = Router();
 
 authRouter.post("/login", authController.loginUser);
-authRouter.post("/login2", authController.loginUser2);
 authRouter.post("/signup", authController.registerUser);
 authRouter.get("/logout", authController.logoutUser);
-authRouter.get("/test", authController.testSQL);
 
 export default authRouter;
