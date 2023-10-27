c29da52011df2df85706fb7da5d7a8733c93f35c
KhalidMohamad <125975613+KhalidMohamad@users.noreply.github.com>
2023-10-27 14:47:00 +0200

    homepage css design

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -3,82 +3,108 @@
 import "./HomePage.css";
 
 function HomePage() {
-
-
-
   return (
     <>
-      <div>
-      <section className="masthead">
-        <div className="container">
-          <div className="masthead-subheading text-uppercase">Willkommen auf der Skii-Erlebnis Seite</div>
-          <div className="masthead-heading ">Lass dich verzaubern!!</div>
-          <button  className="homepage-btn">
-                Log-In
-              </button>
-        </div>
-      </section>
-      <section className="page-section" id="services">
-        <div className="container">
-          <div className="text-center">
-            <h2 className="section-heading text-uppercase">Services</h2>
-            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-          </div>
-          <div className="row text-center">
-            {/* E-Commerce */}
-            <div className="col-md-4">
-              <span className="fa-stack fa-4x">
-                <i className="fas fa-circle fa-stack-2x text-primary"></i>
-                <i className="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 className="my-3">E-Commerce</h4>
-              <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+      <div className="wrapper-container-homepage">
+        <section className="masthead">
+          <div className="container">
+            <div className="masthead-subheading text-uppercase">
+              Willkommen auf der Skii-Erlebnis Seite
             </div>
-            {/* Responsive Design */}
-            <div className="col-md-4">
-              <span className="fa-stack fa-4x">
-                <i className="fas fa-circle fa-stack-2x text-primary"></i>
-                <i className="fas fa-laptop fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 className="my-3">Responsive Design</h4>
-              <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            <div className="masthead-heading ">Lass dich verzaubern!!</div>
+            <button className="homepage-btn">Log-In</button>
+          </div>
+        </section>
+        <section className="page-section" id="services">
+          <div className="container">
+            <div className="text-center">
+              <h2 className="section-heading text-uppercase">Services</h2>
+              <h3 className="section-subheading text-muted">
+                Lorem ipsum dolor sit amet consectetur.
+              </h3>
             </div>
-            {/* Web Security */}
-            <div className="col-md-4">
-              <span className="fa-stack fa-4x">
-                <i className="fas fa-circle fa-stack-2x text-primary"></i>
-                <i className="fas fa-lock fa-stack-1x fa-inverse"></i>
-              </span>
-              <h4 className="my-3">Web Security</h4>
-              <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
+            <div className="row text-center">
+              {/* E-Commerce */}
+              <div className="col-md-4">
+                <span className="fa-stack fa-4x">
+                  <i className="fas fa-circle fa-stack-2x text-primary"></i>
+                  <i className="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                </span>
+                <h4 className="my-3">E-Commerce</h4>
+                <p className="text-muted">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Minima maxime quam architecto quo inventore harum ex magni,
+                  dicta impedit.
+                </p>
+              </div>
+              {/* Responsive Design */}
+              <div className="col-md-4">
+                <span className="fa-stack fa-4x">
+                  <i className="fas fa-circle fa-stack-2x text-primary"></i>
+                  <i className="fas fa-laptop fa-stack-1x fa-inverse"></i>
+                </span>
+                <h4 className="my-3">Responsive Design</h4>
+                <p className="text-muted">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Minima maxime quam architecto quo inventore harum ex magni,
+                  dicta impedit.
+                </p>
+              </div>
+              {/* Web Security */}
+              <div className="col-md-4">
+                <span className="fa-stack fa-4x">
+                  <i className="fas fa-circle fa-stack-2x text-primary"></i>
+                  <i className="fas fa-lock fa-stack-1x fa-inverse"></i>
+                </span>
+                <h4 className="my-3">Web Security</h4>
+                <p className="text-muted">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Minima maxime quam architecto quo inventore harum ex magni,
+                  dicta impedit.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      {/* Other sections go here */}
-    </div>
-    <footer className="footer py-4">
-      <div className="container">
-        <div className="row align-items-center">
-          <div className="col-lg-4 text-lg-start">Copyright &copy; Your Website 2023</div>
-          <div className="col-lg-4 my-3 my-lg-0">
-            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter">
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook">
-              <i className="fab fa-facebook-f"></i>
-            </a>
-            <a className="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn">
-              <i className="fab fa-linkedin-in"></i>
-            </a>
-          </div>
-          <div className="col-lg-4 text-lg-end">
-            <a className="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
-            <a className="link-dark text-decoration-none" href="#!">Terms of Use</a>
-          </div>
-        </div>
+        </section>
+        {/* Other sections go here */}
       </div>
-    </footer>
+      <footer className="footer py-4">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-4 text-lg-start">
+              Copyright &copy; Your Website 2023
+            </div>
+            <div className="col-lg-4 my-3 my-lg-0">
+              <a
+                className="btn btn-dark btn-social mx-2"
+                href="#!"
+                aria-label="Twitter">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a
+                className="btn btn-dark btn-social mx-2"
+                href="#!"
+                aria-label="Facebook">
+                <i className="fab fa-facebook-f"></i>
+              </a>
+              <a
+                className="btn btn-dark btn-social mx-2"
+                href="#!"
+                aria-label="LinkedIn">
+                <i className="fab fa-linkedin-in"></i>
+              </a>
+            </div>
+            <div className="col-lg-4 text-lg-end">
+              <a className="link-dark text-decoration-none me-3" href="#!">
+                Privacy Policy
+              </a>
+              <a className="link-dark text-decoration-none" href="#!">
+                Terms of Use
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
