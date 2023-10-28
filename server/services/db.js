46af5c3d3ba3a973ba690a4a99ef406cf3160949
Sven Grundmann <sgrundmannwbs@gmail.com>
2023-10-28 09:02:44 +0200

    switching db-server hosting

--- server/services/db.js
+++ server/services/db.js
@@ -4,7 +4,7 @@ const { Pool } = pg;
 
 const pool = new Pool({
   host: process.env.DB_HOST,
-  user: process.env.DB_NAME,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
