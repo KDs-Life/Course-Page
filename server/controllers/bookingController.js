2ec6e9cff8008cd9bfb87861cbb77505377e3a16
michamoe <98765003+michamoe@users.noreply.github.com>
2023-10-30 15:49:17 +0100

    check booking quantity

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,5 +1,3 @@
-import Booking from "../models/Booking.js";
-import User from "../models/User.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import pool from "../services/db.js";
 
@@ -163,4 +161,53 @@ export const getBookingsByIDSQL = asyncHandler(async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Booking not found" });
   }
+});
+
+
+// export const createBookingSQL = asyncHandler(async (req, res) => {
+//   const { userId, activityId, quantity, price } = req.body;
+
+//   const userResult = await pool.query("SELECT * FROM users WHERE id = $1;", [userId]);
+//   const activityResult = await pool.query("SELECT * FROM activities WHERE id = $1;", [activityId]);
+
+//   if (userResult.rows.length === 0 || activityResult.rows.length === 0) {
+//     return res.status(404).json({ message: "User or Activity not found" });
+//   }
+//   //return res.json(activityResult.rows[0])
+  
+//   const result = await pool.query(
+//     "INSERT INTO bookings (users_id, activities_id, quantity, price) VALUES ($1, $2, $3, $4) RETURNING *;",
+//     [userId, activityId, quantity, price]
+//   );
+  
+
+//   res.status(201).json(result.rows[0]);
+// });
+
+
+export const createBookingSQL = asyncHandler(async (req, res) => {
+  const { userId, activityId, quantity, price } = req.body;
+
+  const userResult = await pool.query("SELECT * FROM users WHERE id = $1;", [userId]);
+  const activityResult = await pool.query("SELECT * FROM activities WHERE id = $1;", [activityId]);
+
+  if (userResult.rows.length === 0 || activityResult.rows.length === 0) {
+    return res.status(404).json({ message: "User or Activity not found" });
+  }
+
+  const totalQuantityResult = await pool.query(
+    "SELECT b.activities_id, SUM(b.quantity) AS total_quantity, a.maxslots FROM bookings AS b JOIN activities AS a ON b.activities_id = a.id WHERE b.activities_id = $1 GROUP BY b.activities_id, a.maxslots ORDER BY b.activities_id ASC;",
+    [activityId]
+  );
+
+  if (totalQuantityResult.rows[0].total_quantity >= totalQuantityResult.rows[0].maxslots) {
+    return res.status(400).json({ message: "Activity is fully booked" });
+  }
+
+  const result = await pool.query(
+    "INSERT INTO bookings (users_id, activities_id, quantity, price) VALUES ($1, $2, $3, $4) RETURNING *;",
+    [userId, activityId, quantity, price]
+  );
+
+  res.status(201).json(result.rows[0]);
 });
--- server/routes/bookingRouter.js
+++ server/routes/bookingRouter.js
@@ -7,7 +7,7 @@ const bookingRouter = Router();
 //bookingRouter.route("/check/:id").get(bookingController.checkSlotBookingsById);
 //bookingRouter.route("/").get(bookingController.getBookings);
 //bookingRouter.route("/:id").get(bookingController.getBookingById).post(bookingController.createBooking);
-bookingRouter.route("/").get(bookingController.getBookingsSQL);
+bookingRouter.route("/").get(bookingController.getBookingsSQL).post(bookingController.createBookingSQL);
 bookingRouter.route("/:id").get(bookingController.getBookingsByIDSQL);
 
 export default bookingRouter;
