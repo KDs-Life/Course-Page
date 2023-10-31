e2fd7e399e0e174e09b494bdef53a5222a2ee6b8
KhalidMohamad <125975613+KhalidMohamad@users.noreply.github.com>
2023-10-31 10:10:26 +0100

    p tag aktion weg

--- server/index.js
+++ server/index.js
@@ -6,10 +6,7 @@ import authRouter from "./routes/authRouter.js";
 import userRouter from "./routes/userRouter.js";
 import activityRouter from "./routes/activityRouter.js";
 import bookingRouter from "./routes/bookingRouter.js";
-<<<<<<< HEAD
-=======
 import refreshRouter from "./routes/refreshTokenRouter.js";
->>>>>>> 4109db99a04504b334b1c1d8227a8dce9b23c59b
 // import "./db/server.js";
 import "./services/db.js";
 
