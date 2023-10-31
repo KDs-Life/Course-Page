d4d9a48fad558b044bd12998e35d06d84d4c4c27
michamoe <98765003+michamoe@users.noreply.github.com>
2023-10-31 17:25:48 +0100

    getActivitiesFrontpage

--- server/routes/activityRouter.js
+++ server/routes/activityRouter.js
@@ -3,10 +3,8 @@ import * as activityController from "../controllers/activityController.js";
 
 const activityRouter = Router();
 
-//activityRouter.route("/").get(activityController.getActivities);
-//activityRouter.route("/").get(activityController.getActivitiesSQL);
-activityRouter.route("/:id").get(activityController.getActivityByIDSQL);
-activityRouter.route("/").get(activityController.getActivitiesSQL).post(activityController.createActivitySQL);
-//activityRouter.route("/:id").get(activityController.getActivityById).post(activityController.bookActivity);
+activityRouter.route("/frontpage").get(activityController.getRandomActivitiesFrontpage);
+activityRouter.route("/:id").get(activityController.getActivityByID);
+activityRouter.route("/").get(activityController.getActivities).post(activityController.createActivity);
 
 export default activityRouter;
