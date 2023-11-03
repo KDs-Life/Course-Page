aa1e3dcb9979664d8277ed289c1bb87c72eb1fcb
KhalidMohamad <125975613+KhalidMohamad@users.noreply.github.com>
2023-11-03 17:17:47 +0100

    css und code säubern ordnene etc

--- client/src/components/ActivityPage/ActivitiesDetails.jsx
+++ client/src/components/ActivityPage/ActivitiesDetails.jsx
@@ -8,6 +8,7 @@ import {
   parseISO,
 } from "date-fns";
 import "./ActivitiesDetails.css";
+import { Button } from "react-bootstrap";
 
 function ActivitiesDetails() {
   const [activity, setActivity] = useState(null);
@@ -38,36 +39,18 @@ function ActivitiesDetails() {
             alt={activity.image_alt}
             width={`720px`}
           />
-
-          <div className="activity-description">
-            <div className="">
-              <div>{activity.description}</div>
-            </div>
-            <div className="activityDetails-date">
-              {format(parseISO(activity.startdate), "dd.MM.yyyy")}
-              <p>
-                (
-                {formatDistanceToNow(parseISO(activity.startdate), {
-                  addSuffix: true,
-                })}
-                )
-              </p>
-            </div>
-          </div>
           <div className="inner-details">
             <table>
               <tr>
-                <th>Überschrift</th>
-                <th>Eigenschaft</th>
+                <th>Informationen</th>
               </tr>
               <tr>
-                <td>Min. Slots:</td>
-                <td>{activity.minslots}</td>
+                <td>Slots:</td>
+                <td>
+                  {activity.minslots}/{activity.maxslots}
+                </td>
               </tr>
-              <tr>
-                <td>Max. Slots:</td>
-                <td>{activity.maxslots}</td>
-              </tr>
+
               <tr>
                 <td>Price:</td>
                 <td>{activity.price} €</td>
@@ -84,7 +67,23 @@ function ActivitiesDetails() {
                 <td>Published:</td>
                 <td>{activity.published}</td>
               </tr>
+              <Button onClick="" className="booking-btn">
+                BUCHEN DU SAU
+              </Button>
             </table>
+          </div>
+        </div>
+        <div className="activity-description">
+          <div>{activity.description}</div>
+          <div className="activityDetails-date">
+            {format(parseISO(activity.startdate), "dd.MM.yyyy")}
+            <p>
+              (
+              {formatDistanceToNow(parseISO(activity.startdate), {
+                addSuffix: true,
+              })}
+              )
+            </p>
           </div>
         </div>
       </div>
