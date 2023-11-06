a9c983461383cfde626f012b25f5490b92cf1a8b
michamoe <98765003+michamoe@users.noreply.github.com>
2023-11-06 17:07:51 +0100

    activities user bookings

--- server/routes/activityRouter.js
+++ server/routes/activityRouter.js
@@ -6,6 +6,7 @@ const activityRouter = Router();
 activityRouter
   .route("/frontpage")
   .get(activityController.getRandomActivitiesFrontpage);
+activityRouter.route("/:id/bookings").get(activityController.getActivityByIDCheckBookings);
 activityRouter.route("/:id").get(activityController.getActivityByID);
 activityRouter.route("/").get(activityController.getActivities);
 
