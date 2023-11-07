ab3330db5e994c5258fa69f0fa20a3ff6dc54b0e
Sven Grundmann <sgrundmannwbs@gmail.com>
2023-11-07 14:16:17 +0100

    delete own bookings added

--- client/src/components/User/ProfileBookings.jsx
+++ client/src/components/User/ProfileBookings.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-bootstrap";
-import { Button, Table, Alert } from "react-bootstrap";
-import { useState, useEffect } from "react";
+import { Button, Table, Alert, Modal } from "react-bootstrap";
+import { useState, useEffect, useRef } from "react";
 import { ToastContainer, toast, Flip } from "react-toastify";
 import { format, parseISO, formatDistanceToNow } from "date-fns";
 import { useAuth } from "../../context/AuthContext";
@@ -9,8 +9,35 @@ import axios from "../../api/axiosPrivate";
 function ProfileBookings() {
   const { authUser } = useAuth();
   const [bookings, setBookings] = useState();
+  const [show, setShow] = useState(false);
+  const delBooking = useRef();
 
-  //TODO: create delete booking function
+  const cancelDelete = () => setShow(false);
+  const showDelete = (id) => {
+    delBooking.current = id;
+    setShow(true);
+  };
+
+  const deleteBooking = () => {
+    toast.success(`Deleted booking #${delBooking.current}`, {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      theme: "colored",
+    });
+    const deleteBooking = () => {
+      return axios.delete(`/bookings/${delBooking.current}`, {
+        headers: { "Content-type": "application/json" },
+        withCredentials: true,
+      });
+    };
+    deleteBooking();
+    delBooking.current = null;
+    setShow(false);
+  };
 
   const inEuro = (value) => {
     return new Intl.NumberFormat("de-DE", {
@@ -33,7 +60,7 @@ function ProfileBookings() {
       .catch((err) => {
         console.log("ERROR: ", err.message);
       });
-  }, []);
+  }, [bookings]);
 
   if (bookings && bookings.length === 0) {
     return (
@@ -72,7 +99,12 @@ function ProfileBookings() {
                 <td>{inEuro(booking.sum)}</td>
                 <td>
                   <NavLink>
-                    <Button variant="danger">Cancel order</Button>
+                    <Button
+                      variant="danger"
+                      onClick={() => showDelete(booking.id)}
+                    >
+                      Cancel order
+                    </Button>
                   </NavLink>
                 </td>
               </tr>
@@ -92,6 +124,20 @@ function ProfileBookings() {
         pauseOnHover
         theme="colored"
       />
+      <Modal show={show} onHide={cancelDelete}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Booking</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>You are about to delete your booking</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={cancelDelete}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={deleteBooking}>
+            Delete booking
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
